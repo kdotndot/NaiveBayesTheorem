be534e89815e9a6f5b0b414c043b46a31344eff7
kdotndot <69782047+kdotndot@users.noreply.github.com>
2021-10-31 16:33:13 -0700

    categorical good

--- NaiveBayesClassifer.py
+++ NaiveBayesClassifer.py
@@ -1,4 +1,5 @@
 import numpy as np
+import statistics as stat
 
 f = open("testing.txt", "r")
 
@@ -27,42 +28,54 @@ raintoday =[]
 
 f = open("testing.txt", "r")
 
-def addCategorical(dict, val):
+def addCategorical(dict, val, label):
     #Checking if val exists
+    
     if val in dict:
-        dict[val] += 1
+        if label == 'Yes\n':
+            dict[val][0] += 1
+        else:
+            dict[val][1] += 1
     #Value doesn't exist
     else:
-        dict[val] = 1
+        dict[val] = [0,0]
+
+def numericalSplit(list):
+    mean = stat.mean(list)
+    stddev = stat.stdev(list)
+    print(mean)
+    print(stddev)
 
 
 Lines = f.readlines()
 
 for line in Lines:
     x = line.split(", ")
-    addCategorical(loc,x[0])
-    mintemp.append(x[1])
-    maxtemp.append(x[2])
-    rainfall.append(x[3])
-    evap.append(x[4])
-    sunshine.append(x[5])
-    addCategorical(windgusdir,x[6])
-    windgusspeed.append(x[7])
-    addCategorical(winddir9am,x[8])
-    addCategorical(winddir3pm,x[9])
-    windspeed9am.append(x[10])
-    windspeed3pm.append(x[11])
-    hum9am.append(x[12])
-    hum3pm.append(x[13])
-    press9am.append(x[14])
-    press3pm.append(x[15])
-    cloud9am.append(x[16])
-    cloud3pm.append(x[17])
-    temp9am.append(x[18])
-    temp3pm.append(x[19])
-    rainfall.append(x[20])
+    
+    addCategorical(loc,x[0], x[21])
+    
+    mintemp.append(float(x[1]))
+    maxtemp.append(float(x[2]))
+    rainfall.append(float(x[3]))
+    evap.append(float(x[4]))
+    sunshine.append(float(x[5]))
+    addCategorical(windgusdir, x[6], x[21])
+    windgusspeed.append(float(x[7]))
+    addCategorical(winddir9am, x[8], x[21])
+    addCategorical(winddir3pm, x[9], x[21])
+    windspeed9am.append(float(x[10]))
+    windspeed3pm.append(float(x[11]))
+    hum9am.append(float(x[12]))
+    hum3pm.append(float(x[13]))
+    press9am.append(float(x[14]))
+    press3pm.append(float(x[15]))
+    cloud9am.append(float(x[16]))
+    cloud3pm.append(float(x[17]))
+    temp9am.append(float(x[18]))
+    temp3pm.append(float(x[19]))
+    rainfall.append(float(x[20]))
 
-print(winddir9am)
+print(numericalSplit(mintemp))
     
     
 
