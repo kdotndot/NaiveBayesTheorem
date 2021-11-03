d145fc49953f4589ccbc000ee0e6d1c8682963ba
Anh Vu Nguyen <avknguyen9@gmail.com>
2021-11-03 00:37:57 -0700

    Adding Dicts

--- NaiveBayesClassifer.py
+++ NaiveBayesClassifer.py
@@ -4,8 +4,49 @@ import statistics as stat
 f = open("testing.txt", "r")
 
 
+
+numericalDict = {
+    "mintemp" : 1,
+    "maxtemp" : 2,
+    "rainfail" : 3,
+    "evap" : 4,
+    "sunshine" : 5,
+    "windgusspeed" : 7,
+    "windspeed9am" : 10,
+    "windspeed3pm" : 11,
+    "hum9am" : 12,
+    "hum3pm" : 13,
+    "press9am" : 14,
+    "press3pm" : 15,
+    "cloud9am" : 16,
+    "cloud3pm" : 17,
+    "temp9am" : 18,
+    "temp3pm" : 19,
+}
+
+
+featuresMean = {
+    "mintemp" : 0,
+    "maxtemp" : 0,
+    "rainfail" : 0,
+    "evap" : 0,
+    "sunshine" : 0,
+    "windgusspeed" : 0,
+    "windspeed9am" : 0,
+    "windspeed3pm" : 0,
+    "hum9am" : 0,
+    "hum3pm" : 0,
+    "press9am" : 0,
+    "press3pm" : 0,
+    "cloud9am" : 0,
+    "cloud3pm" : 0,
+    "temp9am" : 0,
+    "temp3pm" : 0,
+}
+
+
 loc = {}
-mintemp =[]
+mintemp = [0,0]
 maxtemp =[]
 rainfall =[]
 evap =[] 
@@ -24,7 +65,7 @@ cloud9am =[]
 cloud3pm =[]
 temp9am =[]
 temp3pm =[]
-raintoday =[]
+raintoday ={}
 
 f = open("testing.txt", "r")
 
@@ -38,13 +79,28 @@ def addCategorical(dict, val, label):
             dict[val][1] += 1
     #Value doesn't exist
     else:
-        dict[val] = [0,0]
+        if label == 'Yes\n':
+            dict[val] = [1,0]
+        else:
+            dict[val] = [0,1]
 
-def numericalSplit(list):
-    mean = stat.mean(list)
-    stddev = stat.stdev(list)
-    print(mean)
-    print(stddev)
+def numericalSplit(list, label):
+    if label == 'Yes\n':
+        list[0] += 1
+    else:
+        list[1] += 1
+
+
+def GiniFinder(labels):
+        tot = 0
+        gini = 1
+        for i in range(0,len(labels)):
+            tot += labels[i]
+        if tot == 0:
+            return 1
+        for i in range(0,len(labels)):
+            gini -= (labels[i]/tot)**2
+        return gini
 
 
 Lines = f.readlines()
@@ -53,7 +109,7 @@ for line in Lines:
     x = line.split(", ")
     
     addCategorical(loc,x[0], x[21])
-    
+    #numericalSplit(mintemp,x[21])
     mintemp.append(float(x[1]))
     maxtemp.append(float(x[2]))
     rainfall.append(float(x[3]))
@@ -73,10 +129,10 @@ for line in Lines:
     cloud3pm.append(float(x[17]))
     temp9am.append(float(x[18]))
     temp3pm.append(float(x[19]))
-    rainfall.append(float(x[20]))
+    addCategorical(raintoday,x[20],x[21])
 
-print(numericalSplit(mintemp))
-    
+
+
     
 
 
